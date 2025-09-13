c45c3dc818b4ec1f7b1a6f19a8cf6adcb4f934d3
joelluris <joelluris@gmail.com>
2025-09-13 18:49:33 +0400

    feat: import mangement_groups_level_2

---
+++ import.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.management_groups[0].module.management_groups.azapi_resource.management_groups_level_2["security"]
+  id = "/providers/Microsoft.Management/managementGroups/security"
+}
