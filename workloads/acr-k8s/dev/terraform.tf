cd06ff76926980ee6e9f9a5578dc4fcc97b759f0
joelluris <joelluris@gmail.com>
2025-09-14 20:46:16 +0400

    feat: new workload

---
+++ workloads/acr-k8s/dev/terraform.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = "~> 1.12"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  alias                           = "management"
+  subscription_id                 = try(var.subscription_ids["management"], var.subscription_id_management)
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  alias                           = "connectivity"
+  subscription_id                 = try(var.subscription_ids["connectivity"], var.subscription_id_connectivity)
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azapi" {
+  alias                      = "connectivity"
+  skip_provider_registration = true
+  subscription_id            = try(var.subscription_ids["connectivity"], var.subscription_id_connectivity)
+}
