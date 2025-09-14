b7cca56d4a3559d30ebca6b21f0cddc94ed74914
joelluris <joelluris@gmail.com>
2025-09-14 14:49:37 +0400

    adding development

---
+++ imports.tf
@@ -0,0 +1,4 @@
+# import {
+#   to = azurerm_resource_group.rg_platform_landing_zone.id
+#   id = "/subscriptions/${var.subscription_id_platform}/resourceGroups/${var.platform_landing_zone_resource_group_name}"
+# }
