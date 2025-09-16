f8dd567a714312ddc7ef690954841fddd27be09b
joelluris <69237151+joelluris@users.noreply.github.com>
2025-09-16 13:07:02 +0400

    Update locals.tf

--- workloads/acr-k8s/dev/locals.tf
+++ workloads/acr-k8s/dev/locals.tf
@@ -2,4 +2,6 @@ locals {
   name_replacements = {
     workload = var.resource_name_workload
   }
+
+  resource_names {for k, v in var.resource_name_templates : k => templatestring(value, local.name_replacements)}
 }
