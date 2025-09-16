d019f14d9c582360ab5d23223270c2d1f68eac26
joelluris <69237151+joelluris@users.noreply.github.com>
2025-09-16 13:19:29 +0400

    Update locals.tf

--- workloads/acr-k8s/dev/locals.tf
+++ workloads/acr-k8s/dev/locals.tf
@@ -1,6 +1,9 @@
 locals {
   name_replacements = {
     workload = var.resource_name_workload
+    environment = var.resource_name_environment
+    location = var.location
+    sequence = format("%03d", var.resource_name_sequence_start)
   }
 
   resource_names {for k, v in var.resource_name_templates : k => templatestring(value, local.name_replacements)}
