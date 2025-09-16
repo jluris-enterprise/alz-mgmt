31293b061b9aa964cc3303c783187af7d19dde59
joelluris <69237151+joelluris@users.noreply.github.com>
2025-09-16 16:10:56 +0400

    Update main.tf

--- workloads/acr-k8s/dev/main.tf
+++ workloads/acr-k8s/dev/main.tf
@@ -9,5 +9,7 @@ module "avm-res-resources-resourcegroup" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
   version = "0.2.1"
 
+location = var.location
+name = local.resource_names.
     
 }
