1fe19e716b959f70d20a3e6884f235150c0d215a
joelluris <joelluris@gmail.com>
2025-09-21 09:17:30 +0400

    Update data.tf

--- modules/config-templating/data.tf
+++ modules/config-templating/data.tf
@@ -2,7 +2,6 @@ module "regions" {
   source           = "Azure/avm-utl-regions/azurerm"
   version          = "0.9.0"
   use_cached_data  = false
-  region_filter    = false
   is_recommended   = false
   enable_telemetry = var.enable_telemetry
 }
