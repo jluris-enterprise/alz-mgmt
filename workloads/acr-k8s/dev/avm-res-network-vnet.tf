9064a8ee78289c5d339c8a7eda8dd7d78194d469
joelluris <joelluris@gmail.com>
2025-10-16 08:52:52 +0400

    Update avm-res-network-vnet.tf

--- workloads/acr-k8s/dev/avm-res-network-vnet.tf
+++ workloads/acr-k8s/dev/avm-res-network-vnet.tf
@@ -2,7 +2,8 @@ module "virtual_network" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
   version = "0.15.0"
 
-  resource_group_name = module.resource_group.name
+  # resource_group_name = module.resource_group.name
+  resource_group_id   = module.resource_group.id
   subnets             = local.subnets
   address_space       = [var.address_space]
   location            = var.location
