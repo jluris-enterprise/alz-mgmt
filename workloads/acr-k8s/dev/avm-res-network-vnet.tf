beea51211989dd31f091601f1af81627ea4559a9
joelluris <joelluris@gmail.com>
2025-10-16 09:03:31 +0400

    Update avm-res-network-vnet.tf

--- workloads/acr-k8s/dev/avm-res-network-vnet.tf
+++ workloads/acr-k8s/dev/avm-res-network-vnet.tf
@@ -2,13 +2,12 @@ module "virtual_network" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
   version = "0.15.0"
 
-  # resource_group_name = module.resource_group.name
-  resource_group_id   = module.resource_group.id
-  subnets             = local.subnets
-  address_space       = [var.address_space]
-  location            = var.location
-  name                = local.resource_names.virtual_network_name
-  tags                = var.tags
+  parent_id     = module.resource_group.id
+  subnets       = local.subnets
+  address_space = [var.address_space]
+  location      = var.location
+  name          = local.resource_names.virtual_network_name
+  tags          = var.tags
 
   ddos_protection_plan = {
     enabled = false
