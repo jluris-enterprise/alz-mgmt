1cc3a97e73d912c9dd45f390d4ffad4c760e917e
joelluris <joelluris@gmail.com>
2025-10-16 19:36:22 +0400

    Update avm-res-network-vnet.tf

--- workloads/acr-k8s/dev/avm-res-network-vnet.tf
+++ workloads/acr-k8s/dev/avm-res-network-vnet.tf
@@ -8,9 +8,4 @@ module "virtual_network" {
   location      = var.location
   name          = local.resource_names.virtual_network_name
   tags          = var.tags
-  
-  ddos_protection_plan = {
-    enable = false
-    id = null
-  }
 }
