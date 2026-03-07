23f199edb4ce0466713ef42081e74871ec53e546
joelluris <joelluris@gmail.com>
2026-03-07 07:08:39 +0400

    update dns

--- main.management.runner.vnet.tf
+++ main.management.runner.vnet.tf
@@ -44,7 +44,7 @@ resource "azurerm_network_security_group" "management_runners" {
     source_port_range          = "*"
     destination_port_range     = "53"
     source_address_prefix      = "*"
-    destination_address_prefix = "AzurePlatformDNS"
+    destination_address_prefix = "168.63.129.16"
   }
 
   security_rule {
